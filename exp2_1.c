5ca52f1d879f5da7209ad630a9ae62c37b9a8ace
WhiteWLf-dev <82873634+WhiteWLf-dev@users.noreply.github.com>
2026-01-16 19:51:03 +0300

    Create exp2_1.c

---
+++ exp2_1.c
@@ -0,0 +1,25 @@
+//afl-fuzz -s 0 -i inputs -o outputs -- ./exp2_1 -
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <haproxy/cfgparse.h>
+#include "./src/cfgparse-tcp.c"
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+      char filename[256];
+      sprintf(filename, "exp1_1cfg.%d", getpid()); 
+
+      FILE *fp = fopen(filename, "wb");
+      if (!fp) {
+        exit(1);
+      }
+
+      fwrite(Data, Size, 1, fp);
+      fclose(fp);
+      readcfgfile(filename); //target function
+
+      unlink(filename);
+}
